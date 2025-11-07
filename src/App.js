b656f784efa64def25588c38ccb541aaf0e150ab
BalachandhiranChandru <119665136+BalachandhiranChandru@users.noreply.github.com>
2025-11-07 22:33:34 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// import FullNameDisplay from "./FullNameDisplay";
+import FullNameDisplay from "./FullNameDisplay";
 
 function App() {
   return (
     <div className="App">
-      {/* <FullNameDisplay /> */}
-      <h1>Welcome to the My-App</h1>
+      // {/* <FullNameDisplay /> */}
+      // <h1>Welcome to the My-App</h1>
     </div>
   );
 }
