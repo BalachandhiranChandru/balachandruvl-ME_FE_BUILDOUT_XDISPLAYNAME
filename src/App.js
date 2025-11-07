418b0183f23f4947dc64bd8fbb79a4eaaad51e34
BalachandhiranChandru <119665136+BalachandhiranChandru@users.noreply.github.com>
2025-11-07 22:22:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import FullNameDisplay from "./FullNameDisplay";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <FullNameDisplay /> */}
+      <h1>Welcome to the My-App</h1>
+    </div>
+  );
+}
+
+export default App;
